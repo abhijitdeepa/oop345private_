5a34bdd7ecb0789f349792c4fdf94437d4714923
Abi <86650485+abhijitdeepa@users.noreply.github.com>
2022-07-10 20:57:42 -0400

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,13 +1,4 @@
-// Workshop 6 - STL Containers
-// 2019/10/25 - Cornel
 
-// Name: MinWoo Kim
-// Seneca Student ID: 156417172
-// Seneca email: kim3@myseneca.ca
-// Date of completion: 2019.11.05
-//
-// I confirm that the content of this file is created by me,
-//   with the exception of the parts provided to me by my professor.
 #define CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
